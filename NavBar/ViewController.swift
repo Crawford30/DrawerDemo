dcc19963373ff30b71985be8535ededaebf538e5
joel <joelcrawford30@gmail.com>
2020-02-07 15:24:19 +0300

    drawer

--- NavBar/ViewController.swift
+++ NavBar/ViewController.swift
@@ -13,6 +13,27 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //=======swipe gesture =====
+        
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+        swipeLeft.direction = .left
+        self.view.addGestureRecognizer(swipeLeft)
+
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+        swipeRight.direction = .right
+        self.view.addGestureRecognizer(swipeRight)
+
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+        swipeUp.direction = .up
+        self.view.addGestureRecognizer(swipeUp)
+
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+        swipeDown.direction = .down
+        self.view.addGestureRecognizer(swipeDown)
+        
+        
         // Do any additional setup after loading the view.
         drawerView.layer.cornerRadius = 10.0
         drawerView.clipsToBounds = false     //take the full size of the screen with the items
@@ -27,6 +48,23 @@ class ViewController: UIViewController {
 //        view.frame.origin.y = 65
     
         drawerView.frame = CGRect( x: 0.0 - self.drawerView.frame.size.width, y: 64,  width: self.drawerView.frame.size.width, height: self.drawerView.frame.size.height )
+    }
+    
+    
+    
+    @objc  func handleGesture(gesture: UISwipeGestureRecognizer) -> Void {
+       if gesture.direction == .right {
+            print("Swipe Right")
+       }
+       else if gesture.direction == .left {
+            print("Swipe Left")
+       }
+//       else if gesture.direction == .up {
+//            print("Swipe Up")
+//       }
+//       else if gesture.direction == .down {
+//            print("Swipe Down")
+//       }
     }
     
     //======menu button===
